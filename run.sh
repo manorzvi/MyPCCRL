adccc336ec4e0de9bed70a6caacbf2e374d8ecfb
Manor Zvi <manor.zvi@campus.technion.ac.il>
2021-07-23 13:38:48 +0300

    .

--- run.sh
+++ run.sh
@@ -1,29 +1,41 @@
-mkdir PccNs-v1_0
-cd      PccNs-v1_0
-python ../src/gym/stable_solve.py --env=PccNs-v1 --model-dir=./final_model/ &> out.out &
+#mkdir PccNs-v1_0
+#cd      PccNs-v1_0
+#python ../src/gym/stable_solve.py --env=PccNs-v1 --model-dir=./final_model/ &> out.out &
+#cd ../
+
+#mkdir PccNs-v8_0
+#cd      PccNs-v8_0
+#python ../src/gym/stable_solve.py --env=PccNs-v8 --model-dir=./final_model/ &> out.out &
+#cd ../
+
+#mkdir PccNs-v11_0
+#cd      PccNs-v11_0
+#python ../src/gym/stable_solve_2_senders.py --env=PccNs-v11  --model-dir=./final_model/ &> out.out &
+#cd ../
+
+#mkdir PccNs-v18_0
+#cd      PccNs-v18_0
+#python ../src/gym/stable_solve_2_senders.py --env=PccNs-v18  --model-dir=./final_model/ &> out.out &
+#cd ../
+
+#mkdir PccNs-v19_0
+#cd      PccNs-v19_0
+#python ../src/gym/stable_solve_2_senders.py --env=PccNs-v19  --model-dir=./final_model/ &> out.out &
+#cd ../
+
+#mkdir PccNs-v20_0
+#cd      PccNs-v20_0
+#python ../src/gym/stable_solve_2_senders.py --env=PccNs-v20  --model-dir=./final_model/ &> out.out &
+#cd ../
+
+mkdir PccNs-v21_0
+cd      PccNs-v21_0
+python ../src/gym/stable_solve_2_senders.py --env=PccNs-v21  --model-dir=./final_model/ &> out.out &
 cd ../
 
-mkdir PccNs-v8_0
-cd      PccNs-v8_0
-python ../src/gym/stable_solve.py --env=PccNs-v8 --model-dir=./final_model/ &> out.out &
+mkdir PccNs-v22_0
+cd      PccNs-v22_0
+python ../src/gym/stable_solve_2_senders.py --env=PccNs-v22  --model-dir=./final_model/ &> out.out &
 cd ../
 
-mkdir PccNs-v11_0
-cd      PccNs-v11_0
-python ../src/gym/stable_solve_2_senders.py --env=PccNs-v11  --model-dir=./final_model/ &> out.out &
-cd ../
 
-mkdir PccNs-v18_0
-cd      PccNs-v18_0
-python ../src/gym/stable_solve_2_senders.py --env=PccNs-v18  --model-dir=./final_model/ &> out.out &
-cd ../
-
-mkdir PccNs-v19_0
-cd      PccNs-v19_0
-python ../src/gym/stable_solve_2_senders.py --env=PccNs-v19  --model-dir=./final_model/ &> out.out &
-cd ../
-
-mkdir PccNs-v20_0
-cd      PccNs-v20_0
-python ../src/gym/stable_solve_2_senders.py --env=PccNs-v20  --model-dir=./final_model/ &> out.out &
-cd ../
