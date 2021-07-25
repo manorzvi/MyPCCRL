537b598fae9803903b86454de8a133592d697089
Manor Zvi <manor.zvi@campus.technion.ac.il>
2021-07-25 10:39:24 +0300

    .

---
+++ run_graph_pytorch.sh
@@ -0,0 +1,79 @@
+cd      PccNs-v1_1
+python ../src/gym/graph_run.py pcc_env_log_run_100.json out.out
+python ../src/gym/graph_run.py pcc_env_log_run_200.json out.out
+python ../src/gym/graph_run.py pcc_env_log_run_300.json out.out
+
+python ../src/gym/graph_run.py pcc_env_log_run_16200.json out.out 
+python ../src/gym/graph_run.py pcc_env_log_run_16300.json out.out
+python ../src/gym/graph_run.py pcc_env_log_run_16400.json out.out
+cd ../
+
+cd      PccNs-v8_1
+python ../src/gym/graph_run.py pcc_env_log_run_100.json out.out
+python ../src/gym/graph_run.py pcc_env_log_run_200.json out.out
+python ../src/gym/graph_run.py pcc_env_log_run_300.json out.out
+
+python ../src/gym/graph_run.py pcc_env_log_run_16200.json out.out
+python ../src/gym/graph_run.py pcc_env_log_run_16300.json out.out
+python ../src/gym/graph_run.py pcc_env_log_run_16400.json out.out
+cd ../
+
+cd      PccNs-v11_1
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_100.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_300.json out.out
+
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16300.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16400.json out.out
+cd ../
+
+cd      PccNs-v18_1
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_100.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_300.json out.out
+
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16300.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16400.json out.out
+cd ../
+
+cd      PccNs-v19_1
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_100.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_300.json out.out
+
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16300.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16400.json out.out
+cd ../
+
+cd      PccNs-v20_1
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_100.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_300.json out.out
+
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16300.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16400.json out.out
+cd ../
+
+cd      PccNs-v21_1
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_100.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_300.json out.out
+
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16300.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16400.json out.out
+cd ../
+
+cd      PccNs-v22_1
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_100.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_300.json out.out
+
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16200.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16300.json out.out
+python ../src/gym/graph_run_2_senders.py pcc_env_log_run_16400.json out.out
+cd ../
